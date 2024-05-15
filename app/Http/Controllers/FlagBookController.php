6dc2d7fb194315a23180003163505656f6e92cf0
Yuri Timafejn <timafejn@gmail.com>
2024-05-14 21:21:10 -0300

    feat: created models and controllers for crud

---
+++ app/Http/Controllers/FlagBookController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FlagBook;
+use Illuminate\Http\Request;
+
+class FlagBookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FlagBook $flagBook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FlagBook $flagBook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FlagBook $flagBook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FlagBook $flagBook)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Models\FlagBook;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('livros', BookController::class)->except('edit');
+Route::get('flag/{flag}', [FlagBook::class, 'show'])->name('flag.show');
+
+
